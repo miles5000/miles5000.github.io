bedb2969aaddd249b3397a0f0f12c5bd8df1d72f
miles5000 <73259440+miles5000@users.noreply.github.com>
2020-10-22 15:40:04 +0100

    Update tv.js

--- tv.js
+++ tv.js
@@ -25,8 +25,8 @@
         ];
 
         var tableSchema = {
-            id: "shows",
-            alias: "Top Rated TV Shows",
+            id: "movies",
+            alias: "Top Rated Movies",
             columns: cols
         };
 
@@ -53,7 +53,7 @@
 
     function getResultsPromise(table, pageNum) {
         return new Promise(function(resolve, reject) {
-            var connectionUrl = base_uri + "tv/popular?api_key=" + api_key + "&page=" + pageNum;
+            var connectionUrl = base_uri + "movie/popular?api_key=" + api_key + "&page=" + pageNum;
             
             var xhr = $.ajax({
                 url: connectionUrl,
