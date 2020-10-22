7b92a9d8254a1cca9587237b91092d2c30730df4
miles5000 <73259440+miles5000@users.noreply.github.com>
2020-10-22 17:24:50 +0100

    Update tv.js

--- tv.js
+++ tv.js
@@ -13,6 +13,15 @@
             { id: "poster_path", dataType: tableau.dataTypeEnum.string },
             { id: "popularity", dataType: tableau.dataTypeEnum.float },
             { id: "id", dataType: tableau.dataTypeEnum.int },
+            { id: "backdrop_path", dataType: tableau.dataTypeEnum.string },
+            { id: "vote_average", dataType: tableau.dataTypeEnum.float },
+            { id: "overview", dataType: tableau.dataTypeEnum.string },
+            { id: "first_air_date", dataType: tableau.dataTypeEnum.date },
+            { id: "origin_country", dataType: tableau.dataTypeEnum.string },
+            { id: "original_language", dataType: tableau.dataTypeEnum.string },
+            { id: "vote_count", dataType: tableau.dataTypeEnum.int },
+            { id: "name", dataType: tableau.dataTypeEnum.string },
+            { id: "original_name", dataType: tableau.dataTypeEnum.string }
         ];
 
         var tableSchema = {
@@ -44,7 +53,7 @@
 
     function getResultsPromise(table, pageNum) {
         return new Promise(function(resolve, reject) {
-            var connectionUrl = base_uri + "/movie/popular?api_key=" + api_key + "&page=" + pageNum;
+            var connectionUrl = base_uri + "/tv/top_rated/popular?api_key=" + api_key + "&page=" + pageNum;
             
             var xhr = $.ajax({
                 url: connectionUrl,
@@ -58,6 +67,15 @@
                                 "poster_path": images_uri + record.poster_path,
                                 "popularity": record.popularity,
                                 "id": record.id,
+                                "backdrop_path": images_uri + record.backdrop_path,
+                                "vote_average": record.vote_average,
+                                "overview": record.overview,
+                                "first_air_date": record.first_air_date,
+                                "origin_country": record.origin_country[0] || null,
+                                "original_language": record.original_language,
+                                "vote_count": record.vote_count,
+                                "name": record.name,
+                                "original_name": record.original_name
                             };
 
                             toRet.push(entry)
